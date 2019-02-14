9715d11830a1a3657d11de32dc8864af066a5883
Brian Beeler <brian.beeler@uky.edu>
2019-02-14 08:02:37 -0600

    Update payment-form.js

--- payment-form.js
+++ payment-form.js
@@ -1,41 +1,48 @@
-    setSubtotal();
+    $(document).ready(function() {
+        let paymentType = $('#element_26');
+        let serviceType = $('#element_27');
+        let numOfCans = $('#element_28');
 
-    function setSubtotal() {
+        paymentType.onselect = setSubtotal;
+        serviceType.onselect = setSubtotal;
+        numOfCans.onselect = setSubtotal;
 
-    console.log('set subtotal called');
+        function setSubtotal() {
 
-    const paymentType = $('#element_26');
-    const serviceType = $('#element_27');
-    const numOfCans = $('#element_28');
+            paymentType = $('#element_26');
+            serviceType = $('#element_27');
+            numOfCans = $('#element_28');
 
-    // Calc Subtitle
+            // Calc Subtitle
 
-    const price;
+            let price;
 
-    if (paymentType.value && serviceType.value) {
-        if (serviceType.value === 'Cleaning' && numOfCans.value === '1') {
-            price = 11.95;
-        } else if (serviceType.value === 'Cleaning' && numOfCans.value === '2') {
-            price = 17.95;
-        } else if (serviceType.value === 'Cleaning' && numOfCans.value === '3') {
-            price = 23.95;
-        } else if (serviceType.value === 'VIP' && numOfCans.value === '1') {
-            price = 13.95;
-        } else if (serviceType.value === 'VIP' && numOfCans.value === '2') {
-            price = 20.95;
-        } else if (serviceType.value === 'VIP' && numOfCans.value === '3') {
-            price = 27.95;
+            if (paymentType.value && serviceType.value && numOfCans.value) {
+                if (serviceType.value === 'Cleaning' && numOfCans.value === '1') {
+                    price = 11.95;
+                } else if (serviceType.value === 'Cleaning' && numOfCans.value === '2') {
+                    price = 17.95;
+                } else if (serviceType.value === 'Cleaning' && numOfCans.value === '3') {
+                    price = 23.95;
+                } else if (serviceType.value === 'VIP' && numOfCans.value === '1') {
+                    price = 13.95;
+                } else if (serviceType.value === 'VIP' && numOfCans.value === '2') {
+                    price = 20.95;
+                } else if (serviceType.value === 'VIP' && numOfCans.value === '3') {
+                    price = 27.95;
+                }
+
+                if (paymentType.value === 'Monthly') {
+                    // Do Nothing
+                } else if (paymentType.value === 'Quarterly') {
+                    price *= 3;
+                } else if (paymentType.value === 'Yearly') {
+                    price *= 12;
+                }
+
+                // Show Subtitle
+                $('#li-29').children[0].innerText = "Subtotal: " + price;
+            }
         }
+    });
 
-        if (paymentType.value === 'Monthly') {
-            // Do Nothing
-        } else if (paymentType.value === 'Quarterly') {
-            price *= 3;
-        } else if (paymentType.value === 'Yearly') {
-            price *= 12;
-        }
-
-        // Show Subtitle
-        $('#li-29').children[0].innerText = "Subtotal: " + price;
-    }
-    }
