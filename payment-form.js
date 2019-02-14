dbade99e39ffeca4f3b129d11b293a617111b9d4
Brian Beeler <brian.beeler@uky.edu>
2019-02-14 12:34:28 -0600

    Update payment-form.js

--- payment-form.js
+++ payment-form.js
@@ -52,8 +52,7 @@ $(document).ready(function() {
                     
                 if (price && price > 0) {
                     // Show  paragraph text
-                    $('#li_29').show();
-                    debugger;    
+                    $('#li_29').show(); 
                     $('#li_29')[0].innerText = "Subtotal: $" + price.toFixed(2);
                 } else {
                     $('#li_29').hide();
