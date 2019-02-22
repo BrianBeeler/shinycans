66f112d19b534b77359e278d5b6d76af25df0882
Brian Beeler <brian.beeler@uky.edu>
2019-02-22 09:15:56 -0600

    Update payment-form.js

--- payment-form.js
+++ payment-form.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
         
         function setSubtotal() {
             
-            paymentType = $('#element_26');
-            numOfCans = $('#element_30');
+            paymentType = ($('#element_26') && $('#element_26')[0]) ? $('#element_26')[0] : {};
+            numOfCans = ($('#element_30') && $('#element_30')[0]) ? $('#element_26')[0] : {};           
 
             // Calc Subtitle
             console.log('before price declaration', paymentType, numOfCans);    
